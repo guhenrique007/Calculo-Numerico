d63964277d593ab27b8644770f38eeae408dbc14
Gustavo Henrique Lima <drgustav1997@gmail.com>
2018-09-16 17:40:44 -0300

    Add files via upload

---
+++ secante.m
@@ -0,0 +1,22 @@
+#inputs
+x0 = 3
+x1 = 2
+tolerancia = 0.0005
+iter = 1;
+
+while iter < 7
+  #metodo
+  x = ( (x0 * funcao(x1)) - (x1 * funcao(x0)) )/ (funcao(x1) - funcao(x0)); 
+  #fprintf('A raiz aproximada em %d\n', x );
+  
+  #loop
+  if abs(x - x1) <= tolerancia
+    fprintf('A raiz aproximada em %d\n', x );
+    fprintf('Encontrado com  %d iteracoes\n', iter );
+    break
+  else
+    x0 = x1;
+    x1 = x;
+  endif
+  iter = iter + 1;
+end
