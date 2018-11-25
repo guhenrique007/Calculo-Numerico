ce4d071dbee6313ca162fddfffe9ac7f69975822
Gustavo Henrique Lima <drgustav1997@gmail.com>
2018-11-25 16:46:50 -0200

    Add files via upload

---
+++ EDO.m
@@ -0,0 +1,3 @@
+function edo = EDO(x,y)
+  edo = -1.2 * y + 7 * exp(-0.3 * x);
+end
---
+++ euler.m
@@ -0,0 +1,23 @@
+
+x_0 = 0;
+y_0 = 3;
+x_max = 2.5;
+h = 0.5;
+solucao_exata = [];
+count = 0;
+
+#euller:  y_n+1 = y_n + h * y_linha_n
+fprintf('x_0: %d\n', x_0);
+fprintf('Y_0: %d\n', y_0);
+fprintf('calcular a exata no n=0: %d\n', solucao_exata);
+X = 0;
+Y = 3;
+
+for i = 0:h:x_max 
+  count = count +1;
+  y_linha = EDO(X,Y);
+  X = X + h;
+  Y = Y + (h * y_linha);
+  fprintf('x_: %d = %d\n', count,X);
+  fprintf('y_: %d = %d\n', count,Y);
+endfor
