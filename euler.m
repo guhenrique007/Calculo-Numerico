5b96652d6ea3d1f1fe118277cdf9c444be81d774
Gustavo Henrique Lima <drgustav1997@gmail.com>
2018-12-04 19:17:22 -0200

    Add files via upload

--- euler.m
+++ euler.m
@@ -2,7 +2,7 @@
 x_0 = 0;
 y_0 = 3;
 x_max = 2.5;
-h = 0.5;
+h = 0.1;
 solucao_exata = [];
 count = 0;
 
@@ -12,12 +12,29 @@ fprintf('Y_0: %d\n', y_0);
 fprintf('calcular a exata no n=0: %d\n', solucao_exata);
 X = 0;
 Y = 3;
+x_array = [];
+y_array = [];
 
-for i = 0:h:x_max 
+for i = 0:h:x_max
   count = count +1;
   y_linha = EDO(X,Y);
   X = X + h;
   Y = Y + (h * y_linha);
-  fprintf('x_: %d = %d\n', count,X);
-  fprintf('y_: %d = %d\n', count,Y);
+  fprintf('x_%d = %d  ------------  y_%d = %d\n', count,X,count,Y);
+  x_array = [X x_array];
+  y_array = [Y y_array];
+ 
+
+  #calcular a exata aqui
+  solucao_exata = [y_linha solucao_exata];
+ 
 endfor
+
+#Gráfico
+plot(y_array,"color","b");
+hold on
+  
+plot(x_array,"color","g");
+hold on
+  
+plot(solucao_exata,"color","y");
